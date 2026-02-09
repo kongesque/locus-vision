d6a73772a9653ffdf218f3d42a2b6f3a8ddf87dc
Kongesque <kongesque@gmail.com>
2026-02-09 22:34:35 +0700

    feat: add shadcn components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<Sidebar.Provider>
+	<AppSidebar />
+	<main>
+		<Sidebar.Trigger />
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
